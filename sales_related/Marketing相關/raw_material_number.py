0835c8782f8e654cd417e899ccd92b15aaf7417c
jimmyhsu1010 <jimmyhsu1010@gmail.com>
2023-03-01 23:39:35 +0800

    20230301 解決raw_material_number的問題之一

--- sales_related/Marketing相關/raw_material_number.py
+++ sales_related/Marketing相關/raw_material_number.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright, Playwright, expect
 from selectolax.parser import HTMLParser
 import pandas as pd
+from investiny import historical_data
 
 url = "https://www.investing.com/commodities/brent-oil-historical-data"
 
@@ -10,16 +11,19 @@ def run():
         context = browser.new_context()
         page = context.new_page()
         page.goto(url)
-        # page.wait_for_load_state("networkidle")
-        page.locator(".DatePickerWrapper_icon__Qw9f8").click()
-        page.locator("div.NativeDateInput_root__wbgyP > input[value='2023-02-01']")
-        # page.get_by_role("button", name="Apply").click()
-        # df = pd.read_html(page.content())
-        # print(df)
+        data = pd.read_html(page.content())
+        df = pd.DataFrame(data[1])
+        print(df)
         
-        # context.close()
-        # page.close()
+        context.close()
+        page.close()
+    return df
         
         
 if __name__ == "__main__":
-    run()
+    update_df = run()
+    db = pd.read_csv("sales_related/Marketing相關/Brent Oil Futures Historical Data.csv")
+    result = pd.concat([update_df, db])
+    result.drop_duplicates(subset="Date", keep="first", inplace=True)
+    result.to_csv("sales_related/Marketing相關/Brent Oil Futures Historical Data.csv", index=False)
+    
